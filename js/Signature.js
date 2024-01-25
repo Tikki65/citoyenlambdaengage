5ec90e346a4aa8d8b265a8dd284cd9a1a8ca4fb0
Tikki65 <146470505+Tikki65@users.noreply.github.com>
2024-01-25 09:57:19 +0100

    Update Signature.js

--- js/Signature.js
+++ js/Signature.js
@@ -1,1 +1,9 @@
+class Signature{
+  constructor(canvas){
+    this.canvas = document.querySelector(canvas)
+    this.ctx = this.canvas.getContext("2d")
+    this.ctx.strokeStyle = "blue"
+    this.ctx.lineWidth = 2
+  }
 
+}
