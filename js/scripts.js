c781dce2280c3c08644ab8d6b594d1d9e0eac92d
Tikki65 <146470505+Tikki65@users.noreply.github.com>
2024-01-25 18:34:03 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 window.onload =(){
   let canvas = new Signature("#Signature")
-  
+
+  document.querySelector("effacer").addEventListener("click". (e) =>{
+    e.preventDefault()
+    canvas.effacer()
+  })
 }
